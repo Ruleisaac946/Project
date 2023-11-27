b8014d552abadaff4061504ffc0b5675dcd0758a
Ryan Alam <115599482+rtalam1@users.noreply.github.com>
2023-11-26 19:48:48 -0600

    Styled the post

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
     def index 
-        @posts = Post.order(created_at: :desc)
+        @posts = Post.order(created_at: :asc)
         render :index 
     end 
 end
