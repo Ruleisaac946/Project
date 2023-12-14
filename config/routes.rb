85fd056d0a2f6add77daeb2da647fe2f672ea561
Said Khatib <sbkhatib@memphis.edu>
2023-12-14 01:11:28 -0600

    Created user profile pages with links attached to each post to view profile of the poster

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'users/show'
+
+  resources :users
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # rebase check 
